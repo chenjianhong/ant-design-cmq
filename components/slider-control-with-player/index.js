760c599e10b4be4ae0bf74020e7d0fc5438d3e83
cjh <1815515831@qq.com>
2020-05-02 22:10:42 +0800

    change

--- components/slider-control-with-player/index.js
+++ components/slider-control-with-player/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Row, Col, Button, Slider} from 'antd';
+import 'antd/lib/slider/style/index.css';
+
+
 import styles from './index.less'
 
 
@@ -9,21 +12,68 @@ class SliderControlWithPlayer extends React.Component{
       super(props);
       this.state = {
         playIcon: "caret-right",
-        sliderMax: 0,
         rateVal: 0,
       };
     }
+
+    tick = () => {
+      const { rateVal } = this.state;
+      const { sliderMax, tick:tickMethod, interval=1000 } = this.props;
+  
+      this.timer = setTimeout(() => {
+        if (rateVal >= sliderMax) {
+          console.log('tick clear')
+          clearTimeout(this.timer);
+        } else {
+          this.setState({
+            rateVal:rateVal+1,
+          });
+        }
+        tickMethod(rateVal+1)
+      }, interval);
+    };
+  
+    stopTick = () => {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+    }
     
+    playControl = () => {
+      const { playIcon } = this.state;
+      this.setState({
+        playIcon: playIcon === "pause"?"caret-right":"pause",
+      });
+      if (playIcon === "caret-right") {
+        this.tick()
+      }
+      else {
+        this.stopTick()
+      }
+    };
+
+    onSliderChange = (value) => {
+      const { onSliderChange } = this.props;
+      if (isNaN(value)) {
+        return;
+      }
+      this.setState({
+        rateVal: value,
+      });
+      onSliderChange(value)
+    }
 
   render(){
-    const { playIcon, sliderMax, rateVal } = this.state;
+    const { playIcon, rateVal } = this.state;
+    const { sliderMax=0 } = this.props;
+    console.log('styles.iconScale',styles,styles.iconScale)
     return (
-      <Row gutter={16}  className={styles.iconScale}>
+      <Row gutter={16} className={styles.iconScale} >
         <Col span={1}>
         <Button type="link" shape="circle" icon={playIcon} onClick={this.playControl} />
         </Col>
         <Col span={23}>
-          <Slider className={styles.sliderScale} min={0} max={sliderMax} onChange={this.onSliderChange} value={typeof rateVal === 'number' ? rateVal : 0} />
+          <Slider min={0} max={0} onChange={this.onSliderChange} value={0} />
         </Col>
       </Row>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CleanCSSPlugin = require("less-plugin-clean-css");
+
 
 module.exports = {
   entry: './index.js',
@@ -8,6 +10,9 @@ module.exports = {
     libraryTarget: 'umd',
     globalObject: 'this',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js','.css', '.less'],
   },
   module: {
     rules: [
@@ -19,29 +24,59 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+                ["@babel/plugin-proposal-class-properties", { "loose": true }],
+                ["import", {
+                    "libraryName": "vant",
+                    "libraryDirectory": "es",
+                    "style": true
+                }]
+            ]
           }
         }
       },
       {
         test: /\.less$/,
-        use: [{
+        use: [
+          {
             loader: "style-loader" // creates style nodes from JS strings
-        }, {
-            loader: "css-loader" // translates CSS into CommonJS
-        }, {
-            loader: "less-loader" // compiles Less to CSS
-        }]
-      }
+          }, 
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+            options: {
+              modules: true
+            }
+          }, 
+          {
+            loader: "less-loader", // compiles Less to CSS
+            options: { 
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
+          }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+        ],
+      },
     ]
   },
-    externals: {
-        react : {
-            commonjs: 'react',
-            commonjs2: 'react',
-        },
-        antd : {
-            commonjs: 'antd',
-            commonjs2: 'antd',
-        }
-    }
+  externals: {
+      react : {
+          commonjs: 'react',
+          commonjs2: 'react',
+      },
+      antd : {
+          commonjs: 'antd',
+          commonjs2: 'antd',
+      }
+  }
 };
